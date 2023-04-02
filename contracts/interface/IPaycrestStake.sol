aa2dd385fffc85e185cd0243eaf6bdf685b6baa4
onahprosperity <prosperauthor@gmail.com>
2023-04-03 00:54:51 +0100

    Feat: update protocol main function

--- contracts/interface/IPaycrestStake.sol
+++ contracts/interface/IPaycrestStake.sol
@@ -1,20 +1,12 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.18;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 /**
  * @author Chef Photons, Vaultka Team serving high quality drinks; drink responsibly.
  * Factory and global config params
  */
 interface IPaycrestStake {
     
-    /* ##################################################################
-                                EVENTS
-    ################################################################## */
-
-
-    /* ##################################################################
-                                CUSTOM ERRORS
-    ################################################################## */
+    function rewardValidators(address primaryValidator, address[] memory secondaryValidators, uint256 primaryValidatorsReward, uint256 secondaryValidatorsReward) external returns(bool);
     
 }
