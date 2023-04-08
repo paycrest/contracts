b4717979507bd639411e64c1bb00d1e206ad4779
onahprosperity <prosperauthor@gmail.com>
2023-04-09 00:41:12 +0100

    Feat: update stake fina

--- contracts/PaycrestValidator.sol
+++ contracts/PaycrestValidator.sol
@@ -3,18 +3,15 @@ pragma solidity 0.8.18;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract Paycrest is Ownable { 
+contract PaycrestValidator is Ownable, ReentrancyGuard { 
     using SafeERC20 for IERC20;
-    struct ValidatorInfo {
-        uint256 amount;
-        uint256 rewards;
-    }
     
     address immutable private Paycrest;    
     bool private initialization;
     mapping(address => uint256) private minimumAmount;
-    mapping(address => mapping (address => ValidatorInfo)) private validatorInfo;
+    mapping(address => mapping (address => uint256)) private _balance;
 
     constructor(address _paycrest) {
         Paycrest = _paycrest;
@@ -25,6 +22,12 @@ contract Paycrest is Ownable {
     event Initialized(bool indexed status);
     event Staked(address indexed user, uint256 indexed amount);
     event Withdrawn(address indexed user, uint256 indexed amount);
+    event RewardValidators(
+        bytes32 orderId, 
+        address token, 
+        address indexed validator, 
+        uint256 indexed validatorsReward
+    );
 
     error ThrowInitPaused();
     error TokenNotSupported();
@@ -46,15 +49,40 @@ contract Paycrest is Ownable {
         if(!_isTokenSupported(token)) revert TokenNotSupported();
         if(amount < minimumAmount[token]) revert MinimumRequired();
         IERC20(token).transferFrom(msg.sender, address(this), amount);
-        validatorInfo[msg.sender][token].amount += amount;
+        _balance[msg.sender][token] += amount;
         emit Staked(msg.sender, amount);
     }
 
-    function withdraw(uint256 amount, address token, address recipient) external {
-        uint256 previouslyStakedAmount = validatorInfo[msg.sender][token].amount;
+    function withdraw(uint256 amount, address token, address recipient) external nonReentrant() {
+        uint256 previouslyStakedAmount = _balance[msg.sender][token];
         if(amount > previouslyStakedAmount) revert Insufficient();
-        
+        _balance[msg.sender][token] -= amount;
+        IERC20(token).transfer(recipient, amount);
+        emit Withdrawn(msg.sender, amount);
     }
+
+    function rewardValidators(
+        bytes32 orderId, 
+        address token, 
+        address primaryValidator, 
+        address[] memory secondaryValidators,
+        uint256 primaryValidatorsReward, 
+        uint256 secondaryValidatorsReward
+    ) external returns(bool) {
+        uint256 lengthOfSecondaryValidators = secondaryValidators.length;
+        uint256 secondaryValidatorsShares = secondaryValidatorsReward / lengthOfSecondaryValidators;
+        for(uint256 i = 0; i < lengthOfSecondaryValidators; ) {
+            _balance[secondaryValidators[i]][token] += secondaryValidatorsShares;
+            emit RewardValidators(orderId, token, secondaryValidators[i], secondaryValidatorsShares);
+            unchecked {
+                i++;
+            }
+        }
+        _balance[primaryValidator][token] += primaryValidatorsReward;
+        emit RewardValidators(orderId, token, primaryValidator, primaryValidatorsReward);
+    }
+
+
     function _isTokenSupported(address token) private view returns(bool) {
         (, bytes memory result) = Paycrest.staticcall(abi.encodeWithSignature("isTokenSupported(address)", token));
         return abi.decode(result, (bool));
@@ -72,8 +100,8 @@ contract Paycrest is Ownable {
         return minimumAmount[token];
     }
 
-    function getUserinfo(address user, address token) external view returns(ValidatorInfo memory) {
-        return validatorInfo[user][token];
+    function getValidatorInfo(address user, address token) external view returns(uint256) {
+        return _balance[user][token];
     }
 
 }
