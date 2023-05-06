084400561f6c3bf7735b975ba5bd19b2f163eff9
onahprosperity <prosperauthor@gmail.com>
2023-05-06 14:41:28 +0100

    Fix: git action

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,13 +10,13 @@ module.exports = {
     hardhat: {
       allowUnlimitedContractSize: true,
     },
-    mumbai: {
-      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}}`,
-      accounts: [process.env.pKey],
-      chainId: 80001,
-      USDC_ADDRESS: "0xe6b8a5CF854791412c1f6EFC7CAf629f5Df1c747",
-      saveDeployments: true,
-    },
+    // mumbai: {
+    //   url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}}`,
+    //   accounts: [process.env.pKey],
+    //   chainId: 80001,
+    //   USDC_ADDRESS: "0xe6b8a5CF854791412c1f6EFC7CAf629f5Df1c747",
+    //   saveDeployments: true,
+    // },
   },
   solidity: {
     settings: {
