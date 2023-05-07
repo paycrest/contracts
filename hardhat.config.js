b4c0b9ff04c145d3c34f83fe663b858184039a09
onahprosperity <prosperauthor@gmail.com>
2023-05-07 20:36:37 +0100

    Feat: update config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,46 +3,49 @@ require("@typechain/hardhat");
 require("@nomiclabs/hardhat-ethers");
 
 require("dotenv").config();
+let { DEPLOYER_PRIVATE_KEY } = process.env;
 
-module.exports = {
-  networks: {
-    hardhat: {
-      allowUnlimitedContractSize: true,
-    },
-    mumbai: {
-      url: "https://matic-mumbai.chainstacklabs.com",
-      accounts: [process.env.DEPLOYER_PRIVATE_KEY],
-      chainId: 80001,
-      saveDeployments: true,
-    },
-    bscTestnet: {
-      url: "https://data-seed-prebsc-1-s3.binance.org:8545/",
-      accounts: [process.env.DEPLOYER_PRIVATE_KEY],
-      chainId: 97,
-      saveDeployments: true,
-    },
-  },
-  solidity: {
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-      viaIR: true,
-    },
 
-    compilers: [
-      {
-        version: "0.8.18",
+  module.exports = {
+    networks: {
+      hardhat: {
+        allowUnlimitedContractSize: true,
+        mnemonic: "test test test test test test test test test test test junk",
       },
-      {
-        version: "0.8.9",
+      mumbai: {
+        url: "https://matic-mumbai.chainstacklabs.com",
+        accounts: [DEPLOYER_PRIVATE_KEY],
+        chainId: 80001,
+        saveDeployments: true,
       },
-    ],
-  },
-  etherscan: {
-    apiKey: {
-      // arbitrumGoerli: process.env.ETHERSCAN_KEY,÷      // arbitrumOne: ETHERSCAN_KEY,
+      bscTestnet: {
+        url: "https://data-seed-prebsc-1-s3.binance.org:8545/",
+        accounts: [DEPLOYER_PRIVATE_KEY],
+        chainId: 97,
+        saveDeployments: true,
+      },
     },
-  },
-};
+    solidity: {
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+        viaIR: true,
+      },
+
+      compilers: [
+        {
+          version: "0.8.18",
+        },
+        {
+          version: "0.8.9",
+        },
+      ],
+    },
+    etherscan: {
+      apiKey: {
+        // arbitrumGoerli: process.env.ETHERSCAN_KEY,÷      // arbitrumOne: ETHERSCAN_KEY,
+      },
+    },
+  };
