7ac3bc08f1ce0f807eb5c379b6b32ce42549ef5c
onahprosperity <prosperauthor@gmail.com>
2023-08-12 17:47:35 +0100

    Chore: mint some USDC to deployer and setting manager functions by owner set

--- contracts/mocks/MockUSDC.sol
+++ contracts/mocks/MockUSDC.sol
@@ -5,7 +5,9 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 /// @dev Mock mintable USDC
 contract MockUSDC is ERC20 {
-    constructor() ERC20("MockUDSC", "MUSDC") {}
+    constructor() ERC20("MockUDSC", "MUSDC") {
+        _mint(msg.sender, 1_000_000E18);
+    }
 
     function decimals() public view virtual override returns (uint8) {
         return 6;
--- scripts/paycrest.js
+++ scripts/paycrest.js
@@ -1,18 +1,103 @@
 // scripts/create-box.js
-const { ethers, upgrades } = require("hardhat");
+const { ethers, upgrades, deployments } = require("hardhat");
+const { BigNumber } = require("@ethersproject/bignumber");
 
 async function main() {
-  const USDC_ADDRESS = "0xFda3cD366b8C554Ba2c409d7E810833BB3390946";
-  
+  const { deployer } = await getNamedAccounts();
+
+  const mockUSDCContractInstance = await deployments.deploy("MockUSDC", {
+    contract: "MockUSDC",
+    from: deployer,
+    log: true,
+  });
+  console.log("mockUSDCContractInstance", mockUSDCContractInstance.address);
+
   const Paycrest = await ethers.getContractFactory("Paycrest");
-  const paycrest = await upgrades.deployProxy(Paycrest, [USDC_ADDRESS]);
+  const paycrest = await upgrades.deployProxy(Paycrest, [
+    mockUSDCContractInstance.address,
+  ]);
   console.log("paycrest deployed to:", await paycrest.address);
   console.log("✅ Deployed Paycrest.");
 
-  const PaycrestValidator = await ethers.getContractFactory("PaycrestValidator");
-  const paycrestValidator = await upgrades.deployProxy(PaycrestValidator, [paycrest.address]);
-  console.log("paycrestValidator deployed to:", await paycrestValidator.address);
+  const PaycrestValidator = await ethers.getContractFactory(
+    "PaycrestValidator"
+  );
+  const paycrestValidator = await upgrades.deployProxy(PaycrestValidator, [
+    paycrest.address,
+  ]);
+  console.log(
+    "paycrestValidator deployed to:",
+    await paycrestValidator.address
+  );
   console.log("✅ Deployed paycrestValidator.");
+
+  const protocolFeePercent = BigNumber.from(10_000);
+  const validatorFeePercent = BigNumber.from(5_000); // 5%
+  const usdcMinimumStakeAmount = ethers.utils.parseUnits("500", 6); // not usdc has 6 decimals
+
+  const currency = ethers.utils.formatBytes32String("NGN");
+
+  const firstBank = {
+    code: ethers.utils.formatBytes32String("191"),
+    name: ethers.utils.formatBytes32String("First Bank"),
+  };
+  const opay = {
+    code: ethers.utils.formatBytes32String("192"),
+    name: ethers.utils.formatBytes32String("Opay"),
+  };
+  const palmpay = {
+    code: ethers.utils.formatBytes32String("193"),
+    name: ethers.utils.formatBytes32String("Palmpay Bank"),
+  };
+  const accessBank = {
+    code: ethers.utils.formatBytes32String("194"),
+    name: ethers.utils.formatBytes32String("Access Bank"),
+  };
+  const gtb = {
+    code: ethers.utils.formatBytes32String("195"),
+    name: ethers.utils.formatBytes32String("GTB"),
+  };
+  const stanbic = {
+    code: ethers.utils.formatBytes32String("196"),
+    name: ethers.utils.formatBytes32String("Stanbic IBTC Bank"),
+  };
+
+  await paycrest.setSupportedInstitutions(currency, [
+    firstBank,
+    opay,
+    palmpay,
+    accessBank,
+    gtb,
+    stanbic,
+  ]);
+  console.log(
+    "======================================================= SETTING MANAGER FOR PROTOCOL FEES RECIPIENTS ======================================================="
+  );
+
+  await paycrest.updateProtocolFees(protocolFeePercent, validatorFeePercent);
+
+  console.log(
+    "======================================================= SETTING MANAGER FOR PROTOCOL ADDRESSES ======================================================="
+  );
+  const fee = ethers.utils.formatBytes32String("fee");
+  const aggregatorInit = ethers.utils.formatBytes32String("aggregator");
+  const stakeContract = ethers.utils.formatBytes32String("stakeContract");
+
+  await paycrest.updateFeeRecipient(fee, deployer);
+  await paycrest.updateFeeRecipient(aggregatorInit, deployer);
+  await paycrest.updateFeeRecipient(stakeContract, paycrestValidator.address);
+
+  console.log(
+    "======================================================= SETTING MANAGER FOR MINIMUM AND MAXIMUM ON PAYCREST VALIDATOR======================================================="
+  );
+
+  const whitelist = ethers.utils.formatBytes32String("whitelist");
+  await paycrest.settingManagerBool(whitelist, deployer, true);
+
+  await paycrestValidator.setMinimumAmountForTokens(
+    mockUSDCContractInstance.address,
+    usdcMinimumStakeAmount
+  );
 }
 
 main();
