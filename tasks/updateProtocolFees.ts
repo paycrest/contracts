156233175e28b26d2729a1f9301d40279e1cfef8
onahprosperity <prosperauthor@gmail.com>
2023-09-23 08:18:09 +0100

    feat: added update protocol fees

---
+++ tasks/updateProtocolFees.ts
@@ -0,0 +1,23 @@
+// Import ethers and contracts
+import { ethers } from "ethers";
+import { getContracts } from "./getContracts";
+import { NETWORKS } from "./config";
+
+const chainID = 42161;
+
+const network = NETWORKS[chainID];
+
+async function updateProtocolFees() {
+  // Get contract instances
+  const { paycrestInstance } = await getContracts();
+  const { PROTOCOL_FEE_PERCENT, VALIDATOR_FEE_PERCENT } = network;
+
+  // call contract methods
+  await paycrestInstance.updateProtocolFees(
+    PROTOCOL_FEE_PERCENT,
+    VALIDATOR_FEE_PERCENT
+  );
+}
+
+// Call the function
+updateProtocolFees();
