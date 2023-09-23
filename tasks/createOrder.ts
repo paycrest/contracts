afd44446af8b3899c8263814e82b48e0362d5d57
onahprosperity <prosperauthor@gmail.com>
2023-09-23 18:41:41 +0100

    fix: message hash can be set from config

--- tasks/createOrder.ts
+++ tasks/createOrder.ts
@@ -1,6 +1,9 @@
 // Import ethers and contracts
 import { ethers } from "ethers";
 import { getContracts } from "./getContracts";
+import { NETWORKS } from "./config";
+const chainId = 42161;
+const MESSAGE_HASH = NETWORKS[chainId].MESSAGE_HASH;
 
 async function createOrder() {
   // Get contract instances
@@ -8,10 +11,6 @@ async function createOrder() {
 
   // Define amount
   const amount = ethers.utils.parseUnits("0.1", 15);
-
-  // Define messageHash
-  const messageHash =
-    "0xa3c6bfc43a5f2297001a72039b835698bae96310babf9ff34acc52ad530316f37b961cdf6b119f9422a424b9ad4ac949e282c276131fa7820535a01eb7703cd76350a190e1b6ee4ecc84f6a0f7d090b52e1f565319af139a557fab64b027427e1812576dbfd6c5a2e95166c9a0bc02e967a45be472259572e166758c7865cdc24255f200de23f84f1ac1cc8035b1";
 
   // Call contract method
   await paycrestInstance.createOrder(
@@ -22,7 +21,7 @@ async function createOrder() {
     signer.address,
     0,
     signer.address,
-    messageHash
+    MESSAGE_HASH
   );
 }
 
