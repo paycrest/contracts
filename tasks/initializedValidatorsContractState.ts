e29972d188fd7f19e6c478a5558befb9b5525d4e
onahprosperity <prosperauthor@gmail.com>
2023-09-23 08:38:48 +0100

    feat: update validators contract state via initialized

---
+++ tasks/initializedValidatorsContractState.ts
@@ -0,0 +1,19 @@
+// Import ethers and contracts
+import { getContracts } from "./getContracts";
+import { NETWORKS } from "./config";
+
+const chainID = 42161;
+
+const network = NETWORKS[chainID];
+
+async function initializedValidatorsContractState() {
+  // Get contract instances
+  const { paycrestValidatorInstance } = await getContracts();
+  const { VALIDATORS_STATUS } = network;
+
+  // call contract methods
+  await paycrestValidatorInstance.initialized(VALIDATORS_STATUS);
+}
+
+// Call the function
+initializedValidatorsContractState();
