da2b8184a5f28e998e5feae27657c6cf4353cb37
onahprosperity <prosperauthor@gmail.com>
2023-09-23 08:02:02 +0100

    fix: ethers issue with utils when adding supported institutions

--- tasks/addSupportedInstitutions.ts
+++ tasks/addSupportedInstitutions.ts
@@ -10,10 +10,43 @@ const network = NETWORKS[chainID];
 async function addSupportedInstitutions() {
   // Get contract instances
   const { paycrestInstance } = await getContracts();
-//   console.log(`Adding supported institutions for ${chainID}`);
-//   console.log(network.BANKS);
+  const CURRENCIES = [
+      {
+        code: ethers.utils.formatBytes32String("NGN"),
+        name: ethers.utils.formatBytes32String("Nigerian Naira"),
+      },
+      // Add other currencies here
+    ];
+    const BANKS = [
+      {
+        code: ethers.utils.formatBytes32String("FBNINGLA"),
+        name: ethers.utils.formatBytes32String("First Bank"),
+      },
+      {
+        code: ethers.utils.formatBytes32String("OPAYNINGLA"),
+        name: ethers.utils.formatBytes32String("Opay"),
+      },
+      {
+        code: ethers.utils.formatBytes32String("PPBNINGLA"),
+        name: ethers.utils.formatBytes32String("Palmpay Bank"),
+      },
+      {
+        code: ethers.utils.formatBytes32String("ACBNINGLA"),
+        name: ethers.utils.formatBytes32String("Access Bank"),
+      },
+      {
+        code: ethers.utils.formatBytes32String("GTBNINGLA"),
+        name: ethers.utils.formatBytes32String("GTB"),
+      },
+      {
+        code: ethers.utils.formatBytes32String("IBTCNINGLA"),
+        name: ethers.utils.formatBytes32String("Stanbic IBTC Bank"),
+      },
+      // Add other banks here
+    ];
+    console.log(CURRENCIES);
 
-    await paycrestInstance.setSupportedInstitutions(network.CURRENCIES[0], network.BANKS);
+    await paycrestInstance.setSupportedInstitutions(CURRENCIES[0], BANKS);
 }
 
 // Call the function
