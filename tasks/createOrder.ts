e496907955c887a1510ab8e5bc513fa6ac772eb8
onahprosperity <prosperauthor@gmail.com>
2023-09-23 18:50:51 +0100

    fix: create order params set during config

--- tasks/createOrder.ts
+++ tasks/createOrder.ts
@@ -3,25 +3,32 @@ import { ethers } from "ethers";
 import { getContracts } from "./getContracts";
 import { NETWORKS } from "./config";
 const chainId = 42161;
-const MESSAGE_HASH = NETWORKS[chainId].MESSAGE_HASH;
+const network = NETWORKS[chainId];
 
 async function createOrder() {
   // Get contract instances
   const { signer, paycrestInstance, DERC20Contract } = await getContracts();
 
+  const { CREATE_ORDER_PARAMS } = network;
+
   // Define amount
-  const amount = ethers.utils.parseUnits("0.1", 15);
+  const amount = ethers.utils.parseUnits(
+    CREATE_ORDER_PARAMS.CREATE_ORDER_AMOUNT,
+    15 // IT IS KNOWN THE THE TOKEN DECIMAL IS 18
+  );
 
   // Call contract method
   await paycrestInstance.createOrder(
     DERC20Contract.address,
     amount,
-    ethers.utils.formatBytes32String("FBNINGLA"),
-    970,
+    ethers.utils.formatBytes32String(
+      CREATE_ORDER_PARAMS.CREATE_ORDER_WITH_BANK_CODE
+    ),
+    CREATE_ORDER_PARAMS.CREATE_ORDER_RATE,
     signer.address,
-    0,
+    CREATE_ORDER_PARAMS.SENDER_FEE,
     signer.address,
-    MESSAGE_HASH
+    CREATE_ORDER_PARAMS.MESSAGE_HASH
   );
 }
 
