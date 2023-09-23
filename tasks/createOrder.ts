99f5730b03cb5e0462522c0c78fd4bfa302c5721
onahprosperity <prosperauthor@gmail.com>
2023-09-23 01:35:25 +0100

    chore: revert to using ts for tasks

--- tasks/createOrder.ts
+++ tasks/createOrder.ts
@@ -1,26 +1,30 @@
-const { ethers, upgrades } = require("hardhat");
-const { getContracts } = require("./getContracts.js");
+// Import ethers and contracts
+import { ethers } from "ethers";
+import { getContracts } from "./getContracts";
 
 async function createOrder() {
-  const getContractsInstances = await getContracts();
-  const { signer, paycrestInstance, DERC20_Contract_Instance } =
-    getContractsInstances;
-    
+  // Get contract instances
+  const { signer, paycrestInstance, DERC20Contract } = await getContracts();
+
+  // Define amount
   const amount = ethers.utils.parseUnits("0.1", 15);
 
+  // Define messageHash
   const messageHash =
     "0xa3c6bfc43a5f2297001a72039b835698bae96310babf9ff34acc52ad530316f37b961cdf6b119f9422a424b9ad4ac949e282c276131fa7820535a01eb7703cd76350a190e1b6ee4ecc84f6a0f7d090b52e1f565319af139a557fab64b027427e1812576dbfd6c5a2e95166c9a0bc02e967a45be472259572e166758c7865cdc24255f200de23f84f1ac1cc8035b1";
 
+  // Call contract method
   await paycrestInstance.createOrder(
-    DERC20_Contract_Instance.address,
+    DERC20Contract.address,
     amount,
     ethers.utils.formatBytes32String("FBNINGLA"),
     970,
     signer.address,
     0,
     signer.address,
-    messageHash.toString()
+    messageHash
   );
 }
 
+// Call the function
 createOrder();
--- tasks/getContracts.ts
+++ tasks/getContracts.ts
@@ -1,25 +1,28 @@
-const { ethers, upgrades } = require("hardhat");
-// import contracts abi from artifacts
-const Paycrest = require("../../artifacts/contracts/Paycrest.sol/Paycrest.json");
-const PaycrestValidator = require("../../artifacts/contracts/PaycrestValidator.sol/PaycrestValidator.json");
-const MockUSDC = require("../../artifacts/contracts/mocks/MockUSDC.sol/MockUSDC.json");
-// import dotenv
-require("dotenv").config();
-// import DEPLOYER_PRIVATE_KEY from .env
+// import { ethers } from "hardhat";
+import { ethers } from "ethers";
+import Paycrest from "../artifacts/contracts/Paycrest.sol/Paycrest.json";
+import PaycrestValidator from "../artifacts/contracts/PaycrestValidator.sol/PaycrestValidator.json";
+import MockUSDC from "../artifacts/contracts/mocks/MockUSDC.sol/MockUSDC.json";
+import dotenv from "dotenv";
+import { NETWORKS } from "./config";
+
+dotenv.config();
+
 const { DEPLOYER_PRIVATE_KEY, POLYGON_MUMBAI_RPC_URL } = process.env;
-// import config.js
-const { NETWORKS } = require("../config.js");
 const chainID = 42161;
 
-let network = NETWORKS[chainID];
+const network = NETWORKS[chainID];
 
 async function getContracts() {
-  // get signer
+  if (!DEPLOYER_PRIVATE_KEY) {
+    throw new Error("DEPLOYER_PRIVATE_KEY is undefined");
+  }
+  // Get signer
   const deployer = new ethers.Wallet(DEPLOYER_PRIVATE_KEY);
   const provider = new ethers.providers.JsonRpcProvider(POLYGON_MUMBAI_RPC_URL);
   const signer = deployer.connect(provider);
 
-  // get contract instances
+  // Get contract instances
   const paycrestInstance = new ethers.Contract(
     network.PAYCREST_CONTRACT,
     Paycrest.abi,
@@ -30,7 +33,7 @@ async function getContracts() {
     PaycrestValidator.abi,
     provider
   );
-  const DERC20_Contract_Instance = new ethers.Contract(
+  const DERC20Contract = new ethers.Contract(
     network.DERC20_TOKEN,
     MockUSDC.abi,
     provider
@@ -40,7 +43,8 @@ async function getContracts() {
     signer,
     paycrestInstance,
     paycrestValidatorInstance,
-    DERC20_Contract_Instance,
+    DERC20Contract,
   };
 }
-module.exports = { getContracts };
+
+export { getContracts };
