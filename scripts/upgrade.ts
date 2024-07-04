716086ddb681cddaa11bac080fb94b4f2f55b011
onahprosperity <prosperauthor@gmail.com>
2024-07-04 15:26:14 +0100

    chore: refractor upgrade.ts to heck if signer balance is 0 it should throw

--- scripts/upgrade.ts
+++ scripts/upgrade.ts
@@ -4,17 +4,31 @@ import { NETWORKS } from "./config";
 const networkConfig = NETWORKS[network.config.chainId as keyof typeof NETWORKS];
 
 async function main() {
-  const proxyContractAddress = networkConfig.GATEWAY_CONTRACT;
-  const factory = await ethers.getContractFactory("Gateway");
-  const contract = await upgrades.upgradeProxy(
-    proxyContractAddress,
-    factory
-  );
+	try {
+		const [signer] = await ethers.getSigners(); // Get the signer (the account performing the upgrade)
+		const balance = await signer.getBalance(); // Get the balance of the signer's address
 
-  console.log("✅ Upgraded Gateway: ", contract.address);
+		// Check if the balance is exactly 1 ETH
+		if (balance.eq(0)) {
+			throw new Error("Balance is exactly 1 ETH. Upgrade aborted.");
+		}
+
+		const proxyContractAddress = networkConfig.GATEWAY_CONTRACT;
+		const factory = await ethers.getContractFactory("Gateway");
+		const contract = await upgrades.upgradeProxy(proxyContractAddress, factory);
+
+		console.log("✅ Upgraded Gateway: ", contract.address);
+	} catch (error) {
+		if (error instanceof Error) {
+			console.error("❌ Upgrade failed: ", error.message);
+		} else {
+			console.error("❌ Upgrade failed: Unknown error occurred");
+		}
+		process.exitCode = 1;
+	}
 }
 
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+	console.error(error);
+	process.exitCode = 1;
 });
