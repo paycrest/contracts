130c898c230ec495e4a7f521636cd8aff4f998e5
onahprosperity <prosperauthor@gmail.com>
2024-09-12 16:49:05 +0100

    fix: transfer refund fee to treasury if fee is greater than zero

--- contracts/Gateway.sol
+++ contracts/Gateway.sol
@@ -203,8 +203,10 @@ contract Gateway is IGateway, GatewaySettingManager, PausableUpgradeable {
 		require(!order[_orderId].isRefunded, 'OrderRefunded');
 		require(order[_orderId].protocolFee >= _fee, 'FeeExceedsProtocolFee');
 
-		// transfer refund fee to the treasury
-		IERC20(order[_orderId].token).transfer(treasuryAddress, _fee);
+		if (_fee > 0) {
+			// transfer refund fee to the treasury
+			IERC20(order[_orderId].token).transfer(treasuryAddress, _fee);
+		}
 
 		// reset state values
 		order[_orderId].isRefunded = true;
