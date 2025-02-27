c2a43f40069c3f1e839f9a054636649cfbdc74e3
Prosper <prosperauthor@gmail.com>
2025-02-27 19:02:35 +0000

    fix: update gas fee calculations in scripts and Hardhat configuration

--- scripts/setSupportedTokens.ts
+++ scripts/setSupportedTokens.ts
@@ -14,11 +14,23 @@ async function main() {
 
   // Get the current nonce
   const currentNonce = await wallet.getTransactionCount();
+  // get provider
+  const provider = new ethers.providers.JsonRpcProvider(networkConfig.RPC_URL);
+
+  const maxPriorityFeePerGas = network.config.chainId === 42220 
+    ? ethers.utils.parseUnits("90", "gwei") // Fallback to 30 Gwei
+    : await provider.getGasPrice()
+
+  const maxFeePerGas = network.config.chainId === 42220 
+    ? ethers.utils.parseUnits("120", "gwei")
+    : await provider.getGasPrice()
 
   // Call contract methods
   Object.entries(networkConfig.SUPPORTED_TOKENS).forEach(async ([key, value], index) => {
     const tx = await contractWithSigner.settingManagerBool(token, value, BigNumber.from(1), {
       nonce: currentNonce + index,
+      maxPriorityFeePerGas,
+      maxFeePerGas,
     });
     await tx.wait();
     console.log(`✅ Set token ${key}: ${tx.hash}`);
