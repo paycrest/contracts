e2a533460612a0e98e39a3ffb0853f97d1d4049b
Onah Prosper <prosperauthor@gmail.com>
2026-04-09 11:32:12 +0100

    chore: streamline Hardhat configuration by removing unused Ethers Chai Matchers plugin
    - Commented out the hardhat-ethers-chai-matchers import in hardhat.config.ts to simplify the configuration.
    - Updated the plugins array to exclude the commented-out Ethers Chai Matchers plugin for better clarity.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,8 +3,6 @@ import hardhatToolboxMochaEthers from "@nomicfoundation/hardhat-toolbox-mocha-et
 import hardhatVerify from "@nomicfoundation/hardhat-verify";
 import hardhatEthers from "@nomicfoundation/hardhat-ethers";
 import hardhatTypechain from "@nomicfoundation/hardhat-typechain";
-// import hardhatMocha from "@nomicfoundation/hardhat-mocha";
-import hardhatEthersChaiMatchers from "@nomicfoundation/hardhat-ethers-chai-matchers";
 import hardhatNetworkHelpers from "@nomicfoundation/hardhat-network-helpers";
 
 import dotenv from "dotenv";
@@ -23,7 +21,7 @@ const flattenContracts = task("flat", "Flattens and prints contracts and their d
 
 
 export default defineConfig({
-	plugins: [hardhatToolboxMochaEthers, hardhatVerify, hardhatEthers, hardhatTypechain, hardhatEthersChaiMatchers, hardhatNetworkHelpers],
+	plugins: [hardhatToolboxMochaEthers, hardhatVerify, hardhatEthers, hardhatTypechain, hardhatNetworkHelpers],
 	tasks: [printAccounts, flattenContracts],
 	networks: {
 		// Mainnets
